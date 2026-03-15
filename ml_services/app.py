3a43403c0b3ec6a1006407ce744d445b3e5a8aa2
Vani Arya <vanigreat6@gmail.com>
2026-03-15 17:32:59 +0530

    modified app

--- ml_services/app.py
+++ ml_services/app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Body
 from pydantic import BaseModel
 import pandas as pd
 import joblib
+import traceback
 app = FastAPI()
 model = joblib.load("traffic_density_voting_model-Copy1.joblib")
 class TrafficInput(BaseModel):
@@ -15,22 +16,25 @@ class TrafficInput(BaseModel):
     average_speed_kmph: float
     travel_time_minutes: float
 @app.post("/predict")
-def predict_density(data: TrafficInput):
+def predict_density(data: TrafficInput = Body(...)):
+    try:
+        input_df = pd.DataFrame([{
+            "start_area": data.start_area,
+            "end_area": data.end_area,
+            "distance_km": data.distance_km,
+            "time_of_day": data.time_of_day,
+            "day_of_week": data.day_of_week,
+            "weather_condition": data.weather_condition,
+            "road_type": data.road_type,
+            "average_speed_kmph": data.average_speed_kmph,
+            "travel_time_minutes": data.travel_time_minutes
+        }])
 
-    # Convert JSON → DataFrame with correct column names
-    input_df = pd.DataFrame([{
-        "start_area": data.start_area,
-        "end_area": data.end_area,
-        "distance_km": data.distance_km,
-        "time_of_day": data.time_of_day,
-        "day_of_week": data.day_of_week,
-        "weather_condition": data.weather_condition,
-        "road_type": data.road_type,
-        "average_speed_kmph": data.average_speed_kmph,
-        "travel_time_minutes": data.travel_time_minutes
-    }])
-    prediction = model.predict(input_df)
+        prediction = model.predict(input_df)
+        print(data)
 
-    return {
-        "predicted_density": prediction[0]
-    }
+        return {"predicted_density": prediction[0]}
+
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=str(e))
